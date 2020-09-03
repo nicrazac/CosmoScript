778564ce6e05f0e3a97114b7a4a2039c9363c171
Zac <nicrazac@gmail.com>
2020-09-03 17:00:04 -0400

    Updated plot functionality to add location w/directional buttons

--- kivyApp.py
+++ kivyApp.py
@@ -2,6 +2,8 @@ import sqlite3
 from sqlite3 import Error
 import matplotlib.pyplot as plt
 import numpy as np
+import os.path
+from os import path
 import kivy
 kivy.require('1.11.1')
 from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
@@ -23,30 +25,120 @@ class MainClass(TabbedPanel):
     #Connects ids to kv file
     sizeX = ObjectProperty(None)
     sizeY = ObjectProperty(None)
+    startX = ObjectProperty(None)
+    startY = ObjectProperty(None)
+    gName = ObjectProperty(None)
+    gAuthor = ObjectProperty(None)
     generateMap = ObjectProperty(None)
     plot = ObjectProperty(None)
+    left = ObjectProperty(None)
+    right = ObjectProperty(None)
+    up = ObjectProperty(None)
+    down = ObjectProperty(None)
+    
+    #Initializations
+    location = [0,0]
+    xTicks = 0
+    yTicks = 0
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
     
     #Generates Map
     def genMap(self):
-        try:
-            xRange = int(self.sizeX.text) + 1
-            yRange = int(self.sizeY.text) + 1
-        except:
-            return
-        if int(self.sizeX.text) and int(self.sizeY.text):
-            fig = plt.figure()
-            ax = fig.add_subplot(1,1,1)
-            xTicks = np.arange(0, xRange, 1)
-            yTicks = np.arange(0, yRange, 1)
-            ax.set_xticks(xTicks)
-            ax.set_yticks(yTicks)
-            ax.grid(which='both')
+        if self.gName.text != '' and path.exists(self.gName.text + '.db'):
+            dbName = self.gName.text
+            con = sqlite3.connect(dbName + '.db')
+            cursor = con.cursor()
+            cursor.execute("SELECT sizeX, sizeY, startX, startY FROM setup WHERE id = 1")
+            setupData = cursor.fetchall()[0]
+            xRange = setupData[0]
+            yRange = setupData[1]
+            xStart = setupData[2]
+            yStart = setupData[3]
+            self.location[0] = xStart
+            self.location[1] = yStart
+            self.ids.gridPlot.clear_widgets()
+            #fig = plt.figure()
+            #ax = self.fig.add_subplot(1,1,1)
+            plt.scatter(xStart, yStart)
+            self.xTicks = np.arange(0, xRange, 1)
+            self.yTicks = np.arange(0, yRange, 1)
+            self.ax.set_xticks(self.xTicks)
+            self.ax.set_yticks(self.yTicks)
+            self.ax.grid(which='both')
             self.ids.gridPlot.add_widget(FigureCanvasKivyAgg(plt.gcf()))
+            return
+            
+        if self.gName.text != '' and path.exists(self.gName.text + '.db') == False:
+            try:
+                xRange = int(self.sizeX.text)
+                yRange = int(self.sizeY.text)
+                xStart = int(self.startX.text)
+                yStart = int(self.startY.text)
+                dbName = self.gName.text
+                author = self.gAuthor.text
+            except:
+                return
+            con = sqlite3.connect(dbName + '.db')
+            cursor = con.cursor()
+            cursor.execute("CREATE TABLE IF NOT EXISTS setup(id integer PRIMARY KEY AUTOINCREMENT, title text, author text, sizeX integer, sizeY integer, startX integer, startY integer)")
+            con.commit()
+            cursor.execute("CREATE TABLE IF NOT EXISTS spots(id integer PRIMARY KEY AUTOINCREMENT, xLoc integer, yLoc integer, name text)")
+            con.commit()
+            cursor.execute("INSERT INTO setup (title, author, sizeX, sizeY, startX, startY) VALUES (?,?,?,?,?,?)", (dbName, author, xRange, yRange, xStart, yStart))
+            con.commit()
+            cursor.execute("INSERT INTO spots (xLoc, yLoc) VALUES (?,?)", (xStart, yStart))
+            con.commit()
+            self.location[0] = xStart
+            self.location[1] = yStart
+            self.ids.gridPlot.clear_widgets()
+            #fig = plt.figure()
+            #ax = self.fig.add_subplot(1,1,1)
+            plt.scatter(xStart, yStart)
+            self.xTicks = np.arange(0, xRange, 1)
+            self.yTicks = np.arange(0, yRange, 1)
+            self.ax.set_xticks(self.xTicks)
+            self.ax.set_yticks(self.yTicks)
+            self.ax.grid(which='both')
+            self.ids.gridPlot.add_widget(FigureCanvasKivyAgg(plt.gcf()))
+            return
+    
+    def moveLeft(self):
+        self.ids.gridPlot.clear_widgets()
+        self.location[0] = self.location[0] - 1
+        plt.scatter(self.location[0], self.location[1])
+        self.ax.set_xticks(self.xTicks)
+        self.ax.set_yticks(self.yTicks)
+        self.ids.gridPlot.add_widget(FigureCanvasKivyAgg(plt.gcf()))
+        
+    def moveRight(self):
+        self.ids.gridPlot.clear_widgets()
+        self.location[0] = self.location[0] + 1
+        plt.scatter(self.location[0], self.location[1])
+        self.ax.set_xticks(self.xTicks)
+        self.ax.set_yticks(self.yTicks)
+        self.ids.gridPlot.add_widget(FigureCanvasKivyAgg(plt.gcf()))
+        
+    def moveUp(self):
+        self.ids.gridPlot.clear_widgets()
+        self.location[1] = self.location[1] + 1
+        plt.scatter(self.location[0], self.location[1])
+        self.ax.set_xticks(self.xTicks)
+        self.ax.set_yticks(self.yTicks)
+        self.ids.gridPlot.add_widget(FigureCanvasKivyAgg(plt.gcf()))
+        
+    def moveDown(self):
+        self.ids.gridPlot.clear_widgets()
+        self.location[1] = self.location[1] - 1
+        plt.scatter(self.location[0], self.location[1])
+        self.ax.set_xticks(self.xTicks)
+        self.ax.set_yticks(self.yTicks)
+        self.ids.gridPlot.add_widget(FigureCanvasKivyAgg(plt.gcf()))    
         
 #APP Class
 class MyApp(App):
     def build(self):
-        self.title = "My DB Practice"
+        self.title = "CS"
         return MainClass()
 
 #Runs Loop
