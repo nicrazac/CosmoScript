c84a19295b14240810333c1146bc58f91b68cbee
Zac <nicrazac@gmail.com>
2020-09-04 16:32:44 -0400

    More updates

--- kivyApp.py
+++ kivyApp.py
@@ -35,6 +35,9 @@ class MainClass(TabbedPanel):
     right = ObjectProperty(None)
     up = ObjectProperty(None)
     down = ObjectProperty(None)
+    locationCall = ObjectProperty(None)
+    nameDesc = ObjectProperty(None)
+    lookDesc = ObjectProperty(None)
     
     #Initializations
     location = [0,0]
@@ -42,9 +45,14 @@ class MainClass(TabbedPanel):
     yTicks = 0
     fig = plt.figure()
     ax = fig.add_subplot(1,1,1)
+    tempLoc = ax.plot()
+    permLoc = ax.plot()
+    global dbName
+    
     
     #Generates Map
     def genMap(self):
+        global dbName
         if self.gName.text != '' and path.exists(self.gName.text + '.db'):
             dbName = self.gName.text
             con = sqlite3.connect(dbName + '.db')
@@ -58,15 +66,18 @@ class MainClass(TabbedPanel):
             self.location[0] = xStart
             self.location[1] = yStart
             self.ids.gridPlot.clear_widgets()
-            #fig = plt.figure()
-            #ax = self.fig.add_subplot(1,1,1)
-            plt.scatter(xStart, yStart)
+            cursor.execute("SELECT * FROM spots")
+            spotRes = cursor.fetchall()
+            for point in spotRes:
+                self.permLoc, = self.ax.plot(point[1],point[2],'bo')
+            self.tempLoc, = self.ax.plot(xStart, yStart, 'ro')
             self.xTicks = np.arange(0, xRange, 1)
             self.yTicks = np.arange(0, yRange, 1)
             self.ax.set_xticks(self.xTicks)
             self.ax.set_yticks(self.yTicks)
             self.ax.grid(which='both')
             self.ids.gridPlot.add_widget(FigureCanvasKivyAgg(plt.gcf()))
+            self.locationCall.text = "Current Location: ("+str(self.location[0])+","+str(self.location[1])+")"
             return
             
         if self.gName.text != '' and path.exists(self.gName.text + '.db') == False:
@@ -75,15 +86,15 @@ class MainClass(TabbedPanel):
                 yRange = int(self.sizeY.text)
                 xStart = int(self.startX.text)
                 yStart = int(self.startY.text)
-                dbName = self.gName.text
-                author = self.gAuthor.text
             except:
                 return
+            dbName = self.gName.text
+            author = self.gAuthor.text
             con = sqlite3.connect(dbName + '.db')
             cursor = con.cursor()
             cursor.execute("CREATE TABLE IF NOT EXISTS setup(id integer PRIMARY KEY AUTOINCREMENT, title text, author text, sizeX integer, sizeY integer, startX integer, startY integer)")
             con.commit()
-            cursor.execute("CREATE TABLE IF NOT EXISTS spots(id integer PRIMARY KEY AUTOINCREMENT, xLoc integer, yLoc integer, name text)")
+            cursor.execute("CREATE TABLE IF NOT EXISTS spots(id integer PRIMARY KEY AUTOINCREMENT, xLoc integer, yLoc integer, name text, description text)")
             con.commit()
             cursor.execute("INSERT INTO setup (title, author, sizeX, sizeY, startX, startY) VALUES (?,?,?,?,?,?)", (dbName, author, xRange, yRange, xStart, yStart))
             con.commit()
@@ -92,49 +103,92 @@ class MainClass(TabbedPanel):
             self.location[0] = xStart
             self.location[1] = yStart
             self.ids.gridPlot.clear_widgets()
-            #fig = plt.figure()
-            #ax = self.fig.add_subplot(1,1,1)
-            plt.scatter(xStart, yStart)
+            cursor.execute("SELECT * FROM spots")
+            spotRes = cursor.fetchall()
+            for point in spotRes:
+                self.permLoc, = self.ax.plot(point[1],point[2],'bo')
+            self.tempLoc, = self.ax.plot(xStart, yStart, 'ro')
             self.xTicks = np.arange(0, xRange, 1)
             self.yTicks = np.arange(0, yRange, 1)
             self.ax.set_xticks(self.xTicks)
             self.ax.set_yticks(self.yTicks)
             self.ax.grid(which='both')
             self.ids.gridPlot.add_widget(FigureCanvasKivyAgg(plt.gcf()))
+            self.locationCall.text = "Current Location: ("+str(self.location[0])+","+str(self.location[1])+")"
             return
     
     def moveLeft(self):
         self.ids.gridPlot.clear_widgets()
+        self.tempLoc.remove()
         self.location[0] = self.location[0] - 1
-        plt.scatter(self.location[0], self.location[1])
+        self.tempLoc, = self.ax.plot(self.location[0], self.location[1], 'ro')
         self.ax.set_xticks(self.xTicks)
         self.ax.set_yticks(self.yTicks)
         self.ids.gridPlot.add_widget(FigureCanvasKivyAgg(plt.gcf()))
+        self.locationCall.text = "Current Location: ("+str(self.location[0])+","+str(self.location[1])+")"
+        self.loadSpot(self.location[0],self.location[1])
         
     def moveRight(self):
         self.ids.gridPlot.clear_widgets()
+        self.tempLoc.remove()
         self.location[0] = self.location[0] + 1
-        plt.scatter(self.location[0], self.location[1])
+        self.tempLoc, = self.ax.plot(self.location[0], self.location[1], 'ro')
         self.ax.set_xticks(self.xTicks)
         self.ax.set_yticks(self.yTicks)
         self.ids.gridPlot.add_widget(FigureCanvasKivyAgg(plt.gcf()))
+        self.locationCall.text = "Current Location: ("+str(self.location[0])+","+str(self.location[1])+")"
+        self.loadSpot(self.location[0],self.location[1])
         
     def moveUp(self):
         self.ids.gridPlot.clear_widgets()
+        self.tempLoc.remove()
         self.location[1] = self.location[1] + 1
-        plt.scatter(self.location[0], self.location[1])
+        self.tempLoc, = self.ax.plot(self.location[0], self.location[1], 'ro')
         self.ax.set_xticks(self.xTicks)
         self.ax.set_yticks(self.yTicks)
         self.ids.gridPlot.add_widget(FigureCanvasKivyAgg(plt.gcf()))
+        self.locationCall.text = "Current Location: ("+str(self.location[0])+","+str(self.location[1])+")"
+        self.loadSpot(self.location[0],self.location[1])
         
     def moveDown(self):
         self.ids.gridPlot.clear_widgets()
+        self.tempLoc.remove()
         self.location[1] = self.location[1] - 1
-        plt.scatter(self.location[0], self.location[1])
+        self.tempLoc, = self.ax.plot(self.location[0], self.location[1], 'ro')
         self.ax.set_xticks(self.xTicks)
         self.ax.set_yticks(self.yTicks)
-        self.ids.gridPlot.add_widget(FigureCanvasKivyAgg(plt.gcf()))    
+        self.ids.gridPlot.add_widget(FigureCanvasKivyAgg(plt.gcf()))   
+        self.locationCall.text = "Current Location: ("+str(self.location[0])+","+str(self.location[1])+")"
+        self.loadSpot(self.location[0],self.location[1])
+
+    def loadSpot(self,xLoc,yLoc):
+        global dbName
+        con = sqlite3.connect(dbName + '.db')
+        cursor = con.cursor()
+        cursor.execute("SELECT name, description FROM spots WHERE xLoc = ? AND yLoc = ?", (xLoc, yLoc))
+        dataRes = cursor.fetchall()
+        try:
+            self.nameDesc.text = dataRes[0][0]
+        except:
+            self.nameDesc.text = ''
+        try:
+            self.lookDesc.text = dataRes[0][1]
+        except:
+            self.lookDesc.text = ''
+                
+    def updateName(self):
+        global dbName
+        con = sqlite3.connect(dbName + '.db')
+        cursor = con.cursor()
+        cursor.execute("UPDATE spots SET name = ? WHERE xLoc = ? AND yLoc = ?", (self.nameDesc.text, self.location[0], self.location[1]))
+        con.commit()
         
+    def updateLook(self):
+        global dbName
+        con = sqlite3.connect(dbName + '.db')
+        cursor = con.cursor()
+        cursor.execute("UPDATE spots SET description = ? WHERE xLoc = ? AND yLoc = ?", (self.lookDesc.text, self.location[0], self.location[1]))
+        con.commit()
 #APP Class
 class MyApp(App):
     def build(self):
